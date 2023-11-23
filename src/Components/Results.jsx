e4dd318a9598c8f54833fd3a409bb1eb2ca3f4ec
Ujef55 <ujef.kelawala123@gmail.com>
2023-11-23 16:58:24 +0530

    Fixing result comp, output result data in table

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -3,6 +3,30 @@ import {calculateInvestmentResults} from '../util/investment.js';
 function Results({ userInputData }) {
     const resultsData = calculateInvestmentResults(userInputData);
     console.log(resultsData);
+    return (
+        <table id='result'>
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment Value</th>
+                    <th>Interest</th>
+                    <th>Total Interest</th>
+                    <th>Invested Capital</th>
+                </tr>
+            </thead>
+            <tbody>
+                {resultsData.map(everyYearData => {
+                    return <tr key={everyYearData.year}>
+                        <td>{everyYearData.year}</td>
+                        <td>{everyYearData.valueEndOfYear}</td>
+                        <td>{everyYearData.interest}</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                })}
+            </tbody>
+        </table>
+    )
 }
 
 export default Results;
